d3556e5982d23f0a7d8d80e67348c8678b38b31d
Javier Carrillo <stringparser@gmail.com>
2015-02-05 22:16:17 +0100

    accounts.start

--- accounts/accounts.js
+++ accounts/accounts.js
@@ -1,23 +1,56 @@
+//
+// The Accounts package is a complete solution for accounts
+// managing. Here is what you need to know to get started:
+//
+// - Add the account package you want to use, i.e. github
+//
+//    $ meteor add accounts-github
+//
+// - Install the accounts-ui package
+//    http://docs.meteor.com/#/full/accountsui
+//
+// - Add {{> loginButtons}} to a template
+//   In our case go to "./accounts.html"
+//
+// - Start the meteor server
+//
+//    $ meteor
+//
+// - Go to http://localhost:3000
+//    You see that red button? Niiice
+//
+// - Follow the instructions
+// - Login with your github account
+// - Open the console
+//
+// Of course there is a lot more, you can read it here:
+//  http://docs.meteor.com/#/full/meteor_loginwithexternalservice
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+  Template.users.helpers({
+    user: function(){
+      return Meteor.users.find();
+    },
+    json: function(user){
+      return JSON.stringify(user, null, 2);
     }
   });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.users.find().observe({
+    added: function(user){
+      console.log('New user registered', user);
+    },
+    changed: function(user, userOld){
+      console.log('User changed');
+      console.log('to', user);
+      console.log('from', userOld);
+    },
+    removed: function(user){
+      console.log('User', user);
+      console.log('was removed');
+    }
   });
+
 }
