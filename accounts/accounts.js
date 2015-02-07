d6c3cbc696e98b24f0d0aff6dee68accf200e29c
Javier Carrillo <stringparser@gmail.com>
2015-02-07 20:51:38 +0100

    lint

--- accounts/accounts.js
+++ accounts/accounts.js
@@ -1,31 +1,3 @@
-//
-// The Accounts package is a complete solution for accounts
-// managing. Here is what you need to know to get started:
-//
-// - Add the account package you want to use, i.e. github
-//
-//    $ meteor add accounts-github
-//
-// - Install the accounts-ui package
-//    http://docs.meteor.com/#/full/accountsui
-//
-// - Add {{> loginButtons}} to a template
-//   In our case go to "./accounts.html"
-//
-// - Start the meteor server
-//
-//    $ meteor
-//
-// - Go to http://localhost:3000
-//    You see that red button? Niiice
-//
-// - Follow the instructions
-// - Login with your github account
-// - Open the console
-//
-// Of course there is a lot more, you can read it here:
-//  http://docs.meteor.com/#/full/meteor_loginwithexternalservice
-
 if (Meteor.isClient) {
   Template.users.helpers({
     user: function(){
--- templates/readme.md
+++ templates/readme.md
@@ -1,7 +1,7 @@
 
- # Templates
+# Templates
 
- They are defined in html form using Spacebars, a template language inspirated by [handlerbars](handlebarsjs.com) which similar syntax but  extended capabilities.
+They are defined in html form using Spacebars, a template language inspirated by [handlerbars](handlebarsjs.com) which similar syntax but  extended capabilities.
 
 The way it works is that any file with a `html` extension will be pre-compiled and sent to the client. Well, not any file with this extension. Files placed at /private or /public will not be treated as such. [Read more about file loading in Meteor](http://docs.meteor.com/#/full/structuringyourapp).
 
